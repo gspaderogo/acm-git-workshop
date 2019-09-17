7bdaea65c6f96639f69938e43110fb3748e67c8e
Gilbert Joseph S. Paderogo <gpaderogo@Gilberts-MacBook-Pro.local>
2019-09-16 21:57:26 -0700

    added extra line of code

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ int   main()
     std::cout << "Hello World!\n";
     std::cout << "Hi I'm Gilbert\n";
     std::cout << "I changed this on GitHub\n";
+    std::cout << "I added another line and pushed this to GitHub\n";
     return 0;
 }
